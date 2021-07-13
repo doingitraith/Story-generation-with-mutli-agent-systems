d7b4eac86b057b6eef0b4c5639c24f7c8c6337f9
Jakob Raith <raith.jakob@outlook.com>
2021-07-13 23:39:18 +0200

    refined locations, adjectives and sensing
    added yarnspinner

--- Project/Information Flow Game Mechanic/Assets/Scripts/Game/Item.cs
+++ Project/Information Flow Game Mechanic/Assets/Scripts/Game/Item.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Item : WorldObject
 {
+    private bool _isInInventory;
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
         base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
         base.Update();
     }
